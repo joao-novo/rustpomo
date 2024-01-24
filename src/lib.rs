bb408dc9cc13de09a1ce7d201a8a8e87170c69ad
João Novo <joaonovo.tech@skiff.com>
2024-01-24 19:39:29 +0000

    added basic timer functionality and removed figlet support

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod pomo {
-    use figlet_rs;
-    use std::time::{Duration, Instant};
+    use indicatif::{self, ProgressBar, ProgressStyle};
+    use soloud::*;
+    use std::thread;
+    use std::time::Duration;
     pub enum State {
         Work,
         Break,
@@ -12,7 +14,6 @@ pub mod pomo {
     /// * `work_time` - A 64-bit unsigned integer that holds the duration of the work period in minutes
     /// * `break_time` - A 64-bit unsigned integer that holds the duration of the break period in minutes
     pub fn timer(work_time: u64, break_time: u64) -> fn(Duration) {
-        //TODO add timer functionality
         let mut current_state: Option<State> = None;
         loop {
             current_state = match current_state {
@@ -21,23 +22,39 @@ pub mod pomo {
                 None => Some(State::Work),
             };
 
-            let work_duration = Duration::from_secs(work_time * 60);
-            let break_duration = Duration::from_secs(break_time * 60);
+            let sl = Soloud::default().unwrap();
+            let mut speech = audio::Speech::default();
+            let work_duration = work_time * 60;
+            let break_duration = break_time * 60;
+            // TODO fix speech synthesis
             match current_state {
-                Some(State::Work) => timer_aux(work_duration),
-                Some(State::Break) => timer_aux(break_duration),
+                Some(State::Work) => {
+                    println!("Starting work period!");
+                    timer_aux(work_duration);
+                    speech.set_text("Work period completed").unwrap();
+                    sl.play(&speech);
+                }
+                Some(State::Break) => {
+                    println!("Starting break period!");
+                    timer_aux(break_duration);
+                    speech.set_text("Break period completed").unwrap();
+                    sl.play(&speech);
+                }
                 None => (),
             }
         }
     }
 
-    pub fn timer_aux(duration: Duration) {
-        let start = Instant::now();
-        let font = figlet_rs::FIGfont::standard().unwrap();
-        let mins = duration.as_secs() / 60;
-        let secs = duration.as_secs() % 60;
-        let figure = font.convert(&format!("{}:{}", mins, secs)[..]);
-        println!("{}", figure.unwrap());
-        std::thread::sleep(duration);
+    pub fn timer_aux(duration: u64) {
+        let progress = ProgressBar::new(duration);
+        progress.set_style(
+            ProgressStyle::with_template("[{wide_bar}] [{elapsed_precise}]")
+                .unwrap()
+                .progress_chars("##-"),
+        );
+        for _ in 0..duration {
+            progress.inc(1);
+            thread::sleep(Duration::from_secs(1));
+        }
     }
 }

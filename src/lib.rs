06e85b9b41530c45f8597f2e1508414df11aafd0
João Novo <joaonovo.tech@skiff.com>
2024-02-03 21:23:09 +0000

    added speech synthesis functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 pub mod pomo {
-    use indicatif::{self, ProgressBar, ProgressStyle};
-    use soloud::*;
-    use std::thread;
-    use std::time::Duration;
-    pub enum State {
+    use indicatif::{self, ProgressBar, ProgressStyle}; // Progress bar used for the timer
+    use std::{thread, time::Duration};
+    use tts; // Speech synthesis
+
+    /// Enum that holds each of the possible states
+    ///
+    /// # Variants
+    /// * `Work` - Represents the state in which the timer is in the work period
+    /// * `Break` - Represents the state in which the timer is in the break period
+    enum State {
         Work,
         Break,
     }
@@ -14,41 +19,44 @@ pub mod pomo {
     /// * `work_time` - A 64-bit unsigned integer that holds the duration of the work period in minutes
     /// * `break_time` - A 64-bit unsigned integer that holds the duration of the break period in minutes
     pub fn timer(work_time: u64, break_time: u64) -> fn(Duration) {
+        println!("Welcome to Rustpomo. Your timer will begin in the work period with a duration of {work_time} minutes, followed by a break period of {break_time} minutes. It will keep looping until you close the terminal or kill the process.");
+        thread::sleep(Duration::from_secs(3));
         let mut current_state: Option<State> = None;
         loop {
+            // Handles state-switching
             current_state = match current_state {
                 Some(State::Work) => Some(State::Break),
                 Some(State::Break) => Some(State::Work),
                 None => Some(State::Work),
             };
 
-            let sl = Soloud::default().unwrap();
-            let mut speech = audio::Speech::default();
             let work_duration = work_time * 60;
             let break_duration = break_time * 60;
-            // TODO fix speech synthesis
+            let mut speech = tts::Tts::new(tts::Backends::SpeechDispatcher).unwrap();
+            // Handles timer and speech synthesis
             match current_state {
                 Some(State::Work) => {
                     println!("Starting work period!");
                     timer_aux(work_duration);
-                    speech.set_text("Work period completed").unwrap();
-                    sl.play(&speech);
+                    speech.speak("Work period over", false).unwrap();
                 }
                 Some(State::Break) => {
                     println!("Starting break period!");
                     timer_aux(break_duration);
-                    speech.set_text("Break period completed").unwrap();
-                    sl.play(&speech);
+                    speech.speak("Break period over", false).unwrap();
                 }
                 None => (),
             }
         }
     }
-
-    pub fn timer_aux(duration: u64) {
+    /// Auxiliary function used for the timer itself
+    ///
+    /// # Arguments
+    /// * `duration` - An integer that represents the duration of the timer
+    fn timer_aux(duration: u64) {
         let progress = ProgressBar::new(duration);
         progress.set_style(
-            ProgressStyle::with_template("[{wide_bar}] [{elapsed_precise}]")
+            ProgressStyle::with_template("[{wide_bar}] [{elapsed_precise}]") // Creates a progress bar with the elapsed time to its right
                 .unwrap()
                 .progress_chars("##-"),
         );
